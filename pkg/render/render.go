6da6f929ee00d903597a0113c95d4390df802620
Nicolas <ambroanicolas@gmail.com>
2022-09-26 15:14:59 -0300

    Added dynamic template cache that automatically detects new layouts and templates

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -2,61 +2,79 @@ package render
 
 // All the files for a package must exist on the same directory
 import (
+	"bytes"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
-
-var templateCache = make(map[string]*template.Template)
 
 // RenderTemplate renders a specific html template to the writer w with filename ending in tmpl.
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	// ParseFiles uses the root of the application as the base string.
-	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.gohtml")
-	err := parsedTemplate.Execute(w, nil)
-
+	// Create a template cache.
+	templateCache, err := createTemplateCache()
 	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
+		log.Fatal(err)
 	}
-}
-
-func RenderTemplateFromCache(w http.ResponseWriter, templateName string) {
-	var tmpl *template.Template
-	var err error
-
-	// Check to see if we have the template in our cache and return it. Otherwise, add it to the cache.
-	_, inMap := templateCache[templateName]
-	if !inMap {
-		log.Println("adding template to cache")
-		err = createTemplateCache(templateName)
-		if err != nil {
-			log.Println(err)
-		}
-	} else {
-		log.Println("using cached template")
+	// Get template from cache.
+	templ, templateFound := templateCache[tmpl]
+	if !templateFound {
+		log.Fatal(err)
 	}
 
-	tmpl = templateCache[templateName]
+	// This buffer is arbitrary code to add more error checking below. Not needed.
+	// Without this, you can only error check when writing to w.
+	buf := new(bytes.Buffer)
+	err = templ.Execute(buf, nil)
+	if err != nil {
+		fmt.Println("error executing template:", err)
+	}
 
-	err = tmpl.Execute(w, nil)
+	// Render the template
+	_, err = buf.WriteTo(w)
 	if err != nil {
 		log.Println(err)
 	}
 }
 
-func createTemplateCache(templateName string) error {
-	templates := []string{
-		fmt.Sprintf("./templates/%s", templateName),
-		"./templates/base.layout.gohtml",
-	}
+func createTemplateCache() (map[string]*template.Template, error) {
+	templateCache := map[string]*template.Template{} // Empty map, same as the make function.
 
-	tmpl, err := template.ParseFiles(templates...)
+	// Get all the files name *.fileName.gohtml from ./templates (FULL PATH from root project)
+	// Ex: templates/home.page.gohtml
+	fileNames, err := filepath.Glob("./templates/*.page.gohtml")
 	if err != nil {
-		return err
+		return templateCache, err
 	}
 
-	templateCache[templateName] = tmpl
-	return nil
+	// Range through the received files.
+	for _, fileName := range fileNames {
+		name := filepath.Base(fileName) // Filename MINUS the full path. Ex home.fileName.gohtml.
+
+		// Create template from the current fileName being iterated by the loop.
+		// The line below creates a template with the name I want (home.filename.gohtml for example).
+		// And it also populates it with the corresponding file.
+		templateSet, err := template.New(name).ParseFiles(fileName) // Page is the FULL PATH.
+		if err != nil {
+			return templateCache, err
+		}
+
+		// Search for layouts, that were not searched in the initial filepath.Glob and need to be applied in
+		// each template (since they are a layout).
+		fileNames, err := filepath.Glob("./templates/*layout.gohtml")
+		if err != nil {
+			return templateCache, err
+		}
+		// If any layout fileNames were found, add the layout to the existing templateSet. So it mixes layout + page.
+		if len(fileNames) > 0 {
+			templateSet, err = templateSet.ParseGlob("./templates/*.layout.gohtml")
+			if err != nil {
+				return templateCache, err
+			}
+		}
+
+		templateCache[name] = templateSet
+	}
+	return templateCache, nil
 }
