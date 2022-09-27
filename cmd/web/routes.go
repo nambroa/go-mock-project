836451f19afda0e87e9cf18e590e20541bf7b3d8
Nicolas <ambroanicolas@gmail.com>
2022-09-27 14:32:06 -0300

    Changed router package to allow easy use of middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
 	"github.com/nambroa/go-mock-project/pkg/config"
 	"github.com/nambroa/go-mock-project/pkg/handlers"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
